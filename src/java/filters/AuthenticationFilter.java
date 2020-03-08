f1105957707c6d472644cd19ba1db8c600fc9a44
clarayi <59713032+clarayi@users.noreply.github.com>
2020-03-07 19:01:37 -0700

    Initial Structure Imported to IDE The initial structure of the lab8 project is provided by the instructor.

--- src/java/filters/AuthenticationFilter.java
+++ src/java/filters/AuthenticationFilter.java
@@ -6,9 +6,6 @@
 package filters;
 
 import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -23,41 +20,40 @@ import javax.servlet.http.HttpSession;
  *
  * @author awarsyle
  */
-public class AuthenticationFilter implements Filter {
-    
-    public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain chain)
-            throws IOException, ServletException {
-        
-            // this code will execute before HomeServlet and UsersServlet
-            HttpServletRequest r = (HttpServletRequest)request;
-            HttpSession session = r.getSession();
-            
-            if (session.getAttribute("username") != null) {
-                // if they are authenticated, i.e. have a username in the session,
-                // then allow them to continue on to the servlet
-                chain.doFilter(request, response);
-            } else {
-                // they do not have a username in the sesion
-                // so, send them to login page
-                HttpServletResponse resp = (HttpServletResponse)response;
-                resp.sendRedirect("login");
-            }
-            
-            // this code will execute after HomeServlet and UsersServlet
-            
-            
+public class AuthenticationFilter implements Filter 
+{   
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException 
+    {   
+        // this code will execute before HomeServlet and UsersServlet
+        HttpServletRequest r = (HttpServletRequest)request;
+        HttpSession session = r.getSession();
+
+        if (session.getAttribute("username") != null) 
+        {
+            // if they are authenticated, i.e. have a username in the session,
+            // then allow them to continue on to the servlet
+            chain.doFilter(request, response);
+        } 
+        else 
+        {
+            // they do not have a username in the sesion
+            // so, send them to login page
+            HttpServletResponse resp = (HttpServletResponse)response;
+            resp.sendRedirect("login");
+        }
+
+        // this code will execute after HomeServlet and UsersServlet
     }
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        
+    public void init(FilterConfig filterConfig) throws ServletException 
+    {
+
     }
 
     @Override
-    public void destroy() {
-        
-    }
+    public void destroy() 
+    {
 
-    
+    }
 }
